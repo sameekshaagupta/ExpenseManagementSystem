cac97fdf855baaa37fabaa0cfebe56d82920f718
Sameeksha Gupta <136142214+Sameekksshhaa@users.noreply.github.com>
2024-10-06 16:13:24 +0530

    mongodb setup

--- server.js
+++ server.js
@@ -3,10 +3,25 @@ const cors = require('cors')
 const morgan = require('morgan')
 const dotenv = require('dotenv')
 const colors = require('colors')
-
+//config .env
+dotenv.config();
 //rest object
 const app = express();
 
 //middleware
 app.use(morgan('dev'))
+app.use(express.json())
 app.use(cors())
+
+//routes
+app.get('/', (req,res)=>{
+    res.send('<h1>Helloooo</h1>')
+})
+
+//creation of port
+const PORT = 8080 || process.env.PORT
+
+//listening the server
+app.listen(PORT, ()=>{
+    console.log(`Server running on Port: ${PORT}`)
+})

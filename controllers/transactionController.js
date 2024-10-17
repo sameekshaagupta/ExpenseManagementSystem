97d83fd80c15602e12e70347207dcbef30a823a8
Sameeksha Gupta <136142214+Sameekksshhaa@users.noreply.github.com>
2024-10-17 20:00:00 +0100

    Transaction Controller

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -1,5 +1,23 @@
-const getAllTransaction = () => {}
+const transactionModel = require('../models/transactionModel')
 
-const addTransaction  = () =>{}
+const getAllTransaction = async (req, res) => {
+    try {
+        const transaction = await transactionModel.find({})
+        res.status(200).json(transaction)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json(error)
+    }
+}
+const addTransaction  = async (req, res) =>{
+    try {
+        const newTransaction = new transactionModel(req.body)
+        await newTransaction.save()
+        res.status(201).send('Transaction Created')
+    } catch (error) {
+        console.log(error)
+        res.status(500).json(error);
+    }
+}
 
 module.exports = {getAllTransaction, addTransaction}
